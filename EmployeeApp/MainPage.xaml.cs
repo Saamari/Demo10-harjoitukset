e96d94e2ae8f47dc53e47c8366351c97cf8e0033
Saamari <sampsa_kautto@hotmail.com>
2016-02-10 15:12:28 +0200

    Moro

---
+++ EmployeeApp/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EmployeeApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //change default window startup mode
+            ApplicationView.PreferredLaunchWindowingMode
+                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            //window size
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Debug.WriteLine("name is {0}", NameTextBox.Text);
+            Debug.WriteLine("email is {0}", EmailTextBox.Text);
+            //password
+
+            //admin or user
+            if((bool)normalUserCheckBox.IsChecked)
+                {
+                Debug.WriteLine("Normal User");
+            }
+
+
+
+
+
+        }
+    }
+}
